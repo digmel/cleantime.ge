6ff4e0eaeb1da32a343fc5f0e599c6096c334b58
George <gio.digmel@gmail.com>
2023-02-24 17:49:54 -0600

    chore: initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,18 +3,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <Layout title="home">
-      <div className="flex flex-1 justify-center">
-        <h1 className="text-white ">Welcome</h1>
-
-        <Image
-          src="/assets/image1.png"
-          width={390}
-          height={507}
-          alt="1"
-          quality={30}
-        />
+    <>
+      <div className="flex flex-grow h-screen justify-center items-center">
+        <h1 className="">საიტი მალე დაემატება</h1>
       </div>
-    </Layout>
+    </>
   );
 }
