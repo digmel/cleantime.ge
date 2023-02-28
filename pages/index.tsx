706fc44c7c48475748030582b504b02d3655346a
George <gio.digmel@gmail.com>
2023-02-27 19:57:23 -0600

    chore: added home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
-import { Layout } from "../components";
-import Image from "next/image";
+import { ContactSection } from "../components";
+import { Layout } from "../components/Layout";
 
 export default function Home() {
   return (
-    <>
-      <div className="flex flex-grow h-screen justify-center items-center">
-        <h1 className="">საიტი მალე დაემატება</h1>
-      </div>
-    </>
+    <Layout title="cleantime.ge">
+      <ContactSection />
+    </Layout>
   );
 }
