cb9085786d04a8c5a9573b4ded26fe5bc7571dc8
George <gio.digmel@gmail.com>
2023-02-27 19:57:14 -0600

    chore: added icons

--- icons/index.ts
+++ icons/index.ts
@@ -1,1 +1,6 @@
-export * from "./RocketIcon";
+export * from "./Logo";
+export * from "./InstagramIcon";
+export * from "./FacebookIcon";
+export * from "./EmailIcon";
+export * from "./LocationIcon";
+export * from "./PhoneIcon";
