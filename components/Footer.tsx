4a7bad8ea328361c263bd9878833aa27b2a6162a
George <gio.digmel@gmail.com>
2023-02-27 19:50:26 -0600

    chore: added footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <div className="md:mx-36 mx-12 py-4 flex items-center md:justify-around border-t border-dark-100 border-opacity-10 flex-col md:flex-row">
+    <div className="md:mx-36 py-4 flex items-center md:justify-around border-t border-dark-100 border-opacity-10 flex-col md:flex-row">
       <div className="flex-initial flex justify-around self-center">
         <h1 className="text-sm text-end font-extralight text-dark-100 opacity-50">
           2023 All rights reserved © Clean Time LLC
