5484df0a95704db1c71a45708005d7dacf90ca95
digmel <47932883+digmel@users.noreply.github.com>
2023-03-11 22:55:14 +0000

    Deploying to public from @ digmel/cleantime.ge@ce5878805a6b629be16bbe58854ad1039c8bbb03 🚀

---
+++ _next/static/Fy5M_9Bv-msRW9-co5h3D/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-9dccc63cf79f37e2.js","static/chunks/pages/index-648e6eb20392b8a5.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
