5c3d142e8097342d6d5348351f3b291b065b11dc
George <gio.digmel@gmail.com>
2023-03-11 18:30:23 -0600

    fix: added hover effect on menu items

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,31 +4,38 @@ import { Logo, LogoMobile, MenuIcon } from "../icons";
 import { Text } from "./Text";
 import { Button } from "./Button";
 import { usePlatform } from "../hooks";
+import { useRouter } from "next/router";
 
 export const Header = () => {
   const isMobile = usePlatform();
+  const router = useRouter();
 
+  const goHome = () => {
+    router.push("/");
+  };
   return (
     <>
       {isMobile ? (
         <div className="bg-light py-5 px-6 flex justify-between items-center border-b border-blue-100 border-opacity-50">
-          <LogoMobile />
+          <a className="flex hover:opacity-70" onClick={goHome}>
+            <LogoMobile />
+          </a>
           <div className="flex">
             <MenuIcon />
           </div>
         </div>
       ) : (
         <div className="bg-light py-3 px-48 flex justify-between items-center border-b border-blue-100 border-opacity-50">
-          <div className="flex">
+          <a className="flex hover:opacity-70" onClick={goHome}>
             <Logo />
-          </div>
+          </a>
 
           <div className="md:flex flex-row items-center gap-12">
-            <Link href="/">
+            <Link href="/" className="hover:opacity-70">
               <Text variant="label">სერვისები</Text>
             </Link>
 
-            <Link href="/">
+            <Link href="/" className="hover:opacity-70">
               <Text variant="label">ჩვენს შესახებ</Text>
             </Link>
 
