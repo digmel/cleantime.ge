81404755ee9562f7ff6ddf900b945ee8f2992882
digmel <47932883+digmel@users.noreply.github.com>
2023-03-12 00:07:39 +0000

    Deploying to public from @ digmel/cleantime.ge@07851656c70f6f01a1016c4220e70d75fd3c1e49 🚀

---
+++ _next/static/Ai0ZRiDAYGvpoXU32KLG_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-ac6a7a2f4ed7ec96.js","static/chunks/pages/index-8e0d3148c11dc548.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
